9bd34cefcad2a95b4fb7eff0cb3e366389896dac
NataliaCP <nataliacuevaspizarro@gmail.com>
2023-12-29 16:01:38 +0100

    Finalized acquire packages route- stacks DB and security checks

---
+++ API/Routing/Packages/AcquireCardPackageCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.API.Routing;
+using MTCG.DAL;
+using MTCG.HttpServer.Response;
+using MTCG.HttpServer.Routing;
+using MTCG.HttpServer.Schemas;
+using MTCG.Interfaces;
+using MTCG.Models;
+
+namespace MTCG.API.Routing.Packages
+{
+    internal class AcquireCardPackageCommand : IRouteCommand
+    {
+        private readonly DatabasePackagesDao _dbPackagesDao;
+        private DatabaseCardDao _cardDao;
+        private DatabaseStacksDao _stacksDao;
+        private DatabaseUserDao _userDao;
+        private readonly User _user;
+
+        public AcquireCardPackageCommand(DatabaseCardDao cardDao, DatabasePackagesDao databasePackagesDao, DatabaseStacksDao stacksDao, DatabaseUserDao userDao, User user)
+        {
+            _cardDao = cardDao;
+            _user = user;
+            _dbPackagesDao = databasePackagesDao;
+            _userDao = userDao;
+            _stacksDao = stacksDao;
+        }
+
+        public HttpResponse Execute()
+        {
+            HttpResponse response;
+            if (_user.Coins < 5)
+            {
+                string payload = "Not enough money for buying a card package";
+                response = new HttpResponse(StatusCode.Forbidden, payload:payload);
+                return response;
+            }
+
+            //WHAT IF THERE ARE NO MORE PACKAGES!!!!
+            //GOOD REQUEST
+            Package randomPackage = _dbPackagesDao.PopRandomPackage();
+            if(randomPackage == null)
+            {
+                string payload = "No card package available for buying";
+                response = new HttpResponse(StatusCode.NotFound, payload:payload);
+                return response;
+            }
+            _stacksDao.AddPackageToUser(_user, randomPackage);
+
+            //TODO REMOVE COINS FROM USERS TABLES AFTER THE PURCHASE
+            _userDao.UpdateUserCoins(_user, _user.Coins-5);
+
+            return new HttpResponse(StatusCode.Ok);
+        }
+    }
+}
---
+++ API/Routing/Packages/CreatePackagesCommand.cs
@@ -0,0 +1,93 @@
+﻿using MTCG.DAL;
+using MTCG.HttpServer.Response;
+using MTCG.HttpServer.Routing;
+using MTCG.API.Routing.Packages;
+using MTCG.HttpServer.Schemas;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.Models;
+using MTCG.Interfaces;
+
+namespace MTCG.API.Routing.Packages
+{
+    internal class CreatePackagesCommand :IRouteCommand
+    {
+
+        private readonly DatabasePackagesDao _dbPackagesDao;
+        private DatabaseCardDao _cardDao;
+        private readonly User _user;
+        private readonly List<Card> _cards;
+
+
+        public CreatePackagesCommand(DatabaseCardDao cardDao, DatabasePackagesDao databasePackagesDao, User user, List<Card> packagesContent)
+        {
+            _cardDao = cardDao;
+            _user = user;
+            _dbPackagesDao = databasePackagesDao;
+            _user = user;
+            _cards = packagesContent;
+        }
+
+        public HttpResponse Execute()
+        {
+            HttpResponse response;
+            //check is the user is authorized (admin)
+            if(!_user.IsAdmin)
+            {
+                response = new HttpResponse(StatusCode.Unauthorized);
+                return response;
+            }
+            //check that evry card is valid
+            foreach(Card card in _cards)
+            {
+                if (!card.IsValid())
+                {
+                    response = new HttpResponse(StatusCode.BadRequest);
+                    return response;
+                }
+                if(_cardDao.GetCardbyId(card.Id) != null) 
+                {
+                    string payload = $"Card with ID: {card.Id} already stored in DB\n";
+                    response = new HttpResponse(StatusCode.Conflict, payload:payload);
+                    return response;
+                }
+            }
+            //check correct number of cards
+            if(_cards.Count != 5)
+            {
+                response = new HttpResponse(StatusCode.BadRequest);
+                return response;
+            }
+            //GOOD REQUEST
+            // each card is created in the cards DB
+            _cardDao.CreateCards(_cards);
+            _dbPackagesDao.CreatePackage(_cards);
+
+            return new HttpResponse(StatusCode.Ok);
+
+
+
+            /*
+            try
+            {
+                _dbPackagesDao.CreatePackages(_cardDao);
+
+                response = new HttpResponse(StatusCode.Created);
+            }
+            //Other way to check if the user is already in DB
+            catch (DuplicateNameException)
+            {
+                response = new HttpResponse(StatusCode.Conflict);
+            }
+
+            return response;
+            */
+        }
+
+    }
+    
+}
